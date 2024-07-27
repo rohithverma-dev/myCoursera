31ada0694b0e7ee67be99b1e09769e3592ac61a2
ROHIT VERMA <167316301+rohithverma-dev@users.noreply.github.com>
2024-07-27 13:48:55 +0530

    Update

--- newton-school-server/config/database.js
+++ newton-school-server/config/database.js
@@ -1,8 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  // MONGODB_URI = "mongodb+srv://rv171614:rohitvermaji@cluster0.w3s3rqp.mongodb.net/?retryWrites=true&w=majority"
-  // MONGODB_URI = "mongodb+srv://rv171614:rohitvermaji@cluster0.w3s3rqp.mongodb.net/newtonschool?retryWrites=true"
   mongoose.connect(process.env.MONGO_URI)
   .then((data)=>{
       console.log(`mongoDb connected to server ${data.connection.host}`);
